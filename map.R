9b449a87c3ee64149918030426d47431844b6daf
Sylvain Schmitt <sylvain.m.schmitt@gmail.com>
2025-01-15 17:18:06 +0100

    smaller map with the 4 chosen FG sites

--- map.R
+++ map.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(sf)
 library(ggspatial)
 br <- list(
-  "MG - Fazenda Sao Nicolau (Mato Grosso)" = tibble(lat = -9.855691350801436, 
+  "MG - Fazenda Sao Nicolau\n(Mato Grosso)" = tibble(lat = -9.855691350801436, 
                                       lon = -58.24784096252843),
   "PA - Belterra (Para)" = tibble(lat = -2.663636, 
                           lon = -54.929739),
@@ -15,12 +15,9 @@ br <- list(
   st_as_sf(coords = c("lon", "lat"), crs = 4326) 
 fg <- tibble(
   Site = "FG - French Guiana sites",
-  x = c(350620.95269, 355363.53402, 168169.37966,
-         213649.32011, 330035.98548, 331298.07581),
-  y = c(530957.9664, 514012.23106, 606905.4673,
-         598499.05919, 546583.00355, 535346.50928),
-  loc = c("egyptienne", "kaw", "malgache", 
-          "montagne_fer", "macouria", "tonnegrande")
+  x = c(320007.40379, 350620.95269, 330035.98548, 331298.07581),
+  y = c(540548.78769, 530957.9664, 546583.00355, 535346.50928),
+  loc = c("christine", "egyptienne", "macouria", "tonnegrande")
 ) %>% 
     st_as_sf(coords = c("x", "y"), crs = 32622) %>% 
     st_transform(crs = 4326)
@@ -31,7 +28,7 @@ fg_zoom <- ne_countries(scale = 'medium', type = 'map_units', returnclass = 'sf'
   st_crop(xmin = -54.6028, xmax = -51.6346, ymax = 5.7507, ymin = 2.1122)
 g_sub <- ggplot() + 
     geom_sf(data = fg_zoom, fill = NA, col = "darkgrey") +
-    geom_sf(data = fg, size = 2, colour="black", pch=21, fill = "red") +
+    geom_sf(data = fg, size = .5, colour="black", pch=21, fill = "red") +
     theme_bw() +
     annotation_scale(location = "br") +
   theme_void()
@@ -40,7 +37,7 @@ g <- ggplot() +
   geom_sf(data = sites, size = 3, colour="black",pch=21, aes(fill = Site)) +
   theme_bw() +
   scale_fill_discrete("") +
-  theme(legend.position = "bottom") +
+  theme(legend.position = "bottom", axis.text = element_text(size = 6)) +
   guides(fill = guide_legend(nrow = 4, byrow = TRUE)) +
   annotation_scale(location = "br") +
   annotation_north_arrow(location = "tl",  
@@ -48,4 +45,4 @@ g <- ggplot() +
   annotation_custom(ggplotGrob(g_sub),
                     xmin = -50, xmax = -30, 
                     ymin = 0, ymax = 17)
-ggsave("fig.png", g, width = 4, height = 6)
+ggsave("fig.png", g, width = 3, height = 4.5)
